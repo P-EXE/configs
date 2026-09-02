fb0084dc9fbaef7b3df5fcf2eae1b11add5b3a9a
alice <paul-pexe@proton.me>
2026-09-02 16:39:24 +0200

    docker podman compat issue

--- nix/modules/hosts/blackbox/virtualization.nix
+++ nix/modules/hosts/blackbox/virtualization.nix
@@ -16,7 +16,7 @@
             storageDriver = "btrfs";
           };
           podman = {
-            enable = true;
+            enable = false;
             # Create a `docker` alias for podman, to use it as a drop-in replacement
             dockerCompat = false;
             # Required for containers under podman-compose to be able to talk to each other.
